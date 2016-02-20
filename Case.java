c9bb346e021a3e96056dafd50e16c225e90eaccd
Quentin PROUST <q.proust@gmail.com>
2016-02-20 01:05:51 +0100

    setter and constructor not needed.

--- Case.java
+++ Case.java
@@ -13,39 +13,24 @@ public class Case extends JButton {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Case() {
-
-	}
-
 	public Case(int id) {
-
-
 		this.setText("1");
-
 		this.setId(id);
 		this.setBounds((id % 8) * 82 + 10, (id / 8) * 54 + 10, 70, 40);
 	}
 
 	public char getValue() {
 		return getText().charAt(0);
-		// return getJeton().getValue();
 	}
 
 	public void increaseValue() {
-
 		char currentValue = getValue();
 		currentValue += 1;
 		setText("" + currentValue);
-
 	}
 
 	public void resetValue() {
 		setText("0");
-		// setJeton().setValue(0);
 	}
 
 
